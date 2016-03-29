0cd594dd93ceae28595c8cbc82c207119cb9fa85
Daniel Becker <beckerdaniel.dani@gmail.com>
2016-03-30 01:46:19 +0200

    Implemented the GameState class.

--- include/GameState.h
+++ include/GameState.h
@@ -1,19 +1,69 @@
 #ifndef GAME_STATE_H
 #define GAME_STATE_H
 
+#include <memory>
+
+#include "Board.h"
+
 namespace ttt {
 
+/**
+ * A class that stores the game state.
+ */
 class GameState
 {
-private:
-  std::shared_ptr<Board> m_board;
+public:
+  /**
+   * Constructs a new \c GameState object.
+   *
+   * \param board A smart pointer to the game board.
+   * \param num_of_players The number of players this game has.
+   */
+  GameState(std::shared_ptr<Board> board,
+	    int num_of_players);
 
-  int m_num_of_players;
+  GameState(const GameState& other);
+  GameState(GameState&& other);
+
+  /**
+   * Returns a const (smart) pointer to the game board.
+   *
+   * \return A const (smart) pointer to the game board.
+   */
+  std::shared_ptr<const Board> getBoard() const;
+
+  /**
+   * Returns the number of players.
+   *
+   * \return The number of players.
+   */
+  int getNumberOfPlayers() const;
+
+  /**
+   * Returns the number of the current player
+   * (from 0 to getNumberOfPlayers - 1).
+   *
+   * \return The number of the current player
+   *         (from 0 to getNumberOfPlayers - 1).
+   */
+  int getCurrentPlayer() const;
+
+  /**
+   * Returns the number of steps that have been taken in this game.
+   *
+   * \return The number of steps that have been taken in this game.
+   */
+  int getStepsTaken() const;
+  
+private:
+  const std::shared_ptr<Board> m_board;
+
+  const int m_num_of_players;
   int m_current_player; // From 0 to m_num_of_players - 1.
 
   int m_steps_taken;
 };
 
-}
+} // namespace ttt.
 
 #endif // GAME_STATE_H
--- src/GameState.cpp
+++ src/GameState.cpp
@@ -1,1 +1,50 @@
 #include "GameState.h"
+
+namespace ttt {
+
+GameState::GameState(std::shared_ptr<Board> board,
+		     int num_of_players)
+  : m_board(board),
+    m_num_of_players(num_of_players),
+    m_current_player(0),
+    m_steps_taken(0)
+{
+}
+
+GameState::GameState(const GameState& other)
+  : m_board(other.m_board->clone()),
+    m_num_of_players(other.m_num_of_players),
+    m_current_player(other.m_current_player),
+    m_steps_taken(other.m_steps_taken)
+{
+}
+
+GameState::GameState(GameState&& other)
+  : m_board(other.m_board),
+    m_num_of_players(other.m_num_of_players),
+    m_current_player(other.m_current_player),
+    m_steps_taken(other.m_steps_taken)
+{
+}
+
+std::shared_ptr<const Board> GameState::getBoard() const
+{
+  return m_board;
+}
+
+int GameState::getNumberOfPlayers() const
+{
+  return m_num_of_players;
+}
+
+int GameState::getCurrentPlayer() const
+{
+  return m_current_player;
+}
+
+int GameState::getStepsTaken() const
+{
+  return m_steps_taken;
+}
+
+} // namespace ttt.
