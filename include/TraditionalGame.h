c5f8a10ccdf8253ff56c2b419bbbc7444db7ffae
Daniel Becker <beckerdaniel.dani@gmail.com>
2016-04-03 04:44:46 +0200

    Some work on TraditionalGame.

--- include/TraditionalGame.h
+++ include/TraditionalGame.h
@@ -2,6 +2,7 @@
 #define TRADITIONAL_GAME_H
 
 #include "Game.h"
+#include "Vec2.h"
 
 namespace ttt {
 
@@ -9,7 +10,8 @@ class TraditionalGame : public Game
 {
 public:
   TraditionalGame(std::shared_ptr<Board> board,
-		  int num_of_players);
+		  int num_of_players,
+		  int length_to_win);
   TraditionalGame(const TraditionalGame& other);
   TraditionalGame(TraditionalGame&& other);
   virtual ~TraditionalGame();
@@ -20,6 +22,13 @@ public:
 			  std::vector<Vec2>& winner_positions) const override;
 
   virtual std::shared_ptr<Game> clone() const override;
+
+  virtual bool isGameWonAt(const Vec2& pos,
+			   int& winner,
+			   std::vector<Vec2>& winner_positions) const;
+
+private:
+  const int m_length_to_win;
 };
 
 } // namespace ttt.
--- src/TraditionalGame.cpp
+++ src/TraditionalGame.cpp
@@ -1,20 +1,64 @@
 #include "TraditionalGame.h"
 
+#include <algorithm>
+
+
+namespace {
+
+bool checkGameWonInDirection(const ttt::Vec2& pos,
+			     int& winner,
+			     std::vector<ttt::Vec2>& winner_positions,
+			     const ttt::Vec2& dir,
+			     std::shared_ptr<const ttt::Board> board,
+			     unsigned int length_to_win)
+{
+  using namespace ttt;
+  
+  winner = board->get(pos);
+  if(winner == 0)
+    return false;
+
+  winner_positions.clear();
+  winner_positions.push_back(pos);
+  
+  Vec2 forward_pos = pos + dir;
+  for (; board->get(forward_pos) == winner; forward_pos += dir)
+  {
+    winner_positions.push_back(forward_pos);
+  }
+
+  std::reverse(winner_positions.begin(), winner_positions.end());
+  
+  Vec2 backward_pos = pos - dir;
+  for (; board->get(backward_pos) == winner; backward_pos -= dir)
+  {
+    winner_positions.push_back(backward_pos);
+  }
+
+  return winner_positions.size() >= length_to_win;
+}
+
+}
+
 namespace ttt {
 
 TraditionalGame::TraditionalGame(std::shared_ptr<Board> board,
-				 int num_of_players)
-  : Game(board, num_of_players)
+				 int num_of_players,
+				 int length_to_win)
+  : Game(board, num_of_players),
+    m_length_to_win(length_to_win)
 {
 }
 
 TraditionalGame::TraditionalGame(const TraditionalGame& other)
-  : Game(other)
+  : Game(other),
+    m_length_to_win(other.m_length_to_win)
 {
 }
 
 TraditionalGame::TraditionalGame(TraditionalGame&& other)
-  : Game(other)
+  : Game(other),
+    m_length_to_win(other.m_length_to_win)
 {
 }
 
@@ -37,6 +81,25 @@ bool TraditionalGame::isGameOver(int& winner,
 std::shared_ptr<Game> TraditionalGame::clone() const
 {
   return std::make_shared<TraditionalGame>(*this);
+}
+
+bool TraditionalGame::isGameWonAt(const Vec2& pos,
+		 int& winner,
+		 std::vector<Vec2>& winner_positions) const
+{
+  static const std::vector<Vec2> directions {Vec2(1, 0), Vec2(0, 1),
+                                             Vec2(1, 1), Vec2(1, -1)};
+  std::shared_ptr<const Board> board = getBoard();
+  
+  for (const Vec2& dir : directions)
+  {
+    bool won = checkGameWonInDirection(pos, winner, winner_positions,
+				       dir, board, m_length_to_win);
+    if (won)
+      return true;
+  }
+
+  return false;
 }
 
 } // namespace ttt.
